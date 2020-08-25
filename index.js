4b3562becc73ea3dd5786e0179a02b922c057087
Luca <lucakanyo@protonmail.com>
2020-08-25 18:05:24 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(string){
+  let hour = parseInt(string.split(':')[0])
+  if (hour < 12){
+    return "Good Morning"
+  } else if (11 < hour && hour <= 16){
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+
+  }
+
+};
+
+function displayMessage(string){
+  document.getElementById('greeting').innerText = string
+
+};
+
+
