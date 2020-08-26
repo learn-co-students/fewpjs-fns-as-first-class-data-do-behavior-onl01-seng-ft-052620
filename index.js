0551d57882610ac7814e452d0307e58baa4ec8c7
SWFullPotential <swfullpotential@gmail.com>
2020-08-26 11:04:22 -0600

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+function greet(string) {
+  let currentTime = string.split(':')[0];
+  currentTime = parseInt(currentTime)
+  console.log(currentTime)
+  if (currentTime < 12) {
+  return "Good Morning"
+  }
+  else if (12 <= currentTime && currentTime < 17) {
+    return "Good Afternoon"
+  }
+  else if (currentTime >= 17) {
+    return "Good Evening"
+  }
+};
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message
+}
