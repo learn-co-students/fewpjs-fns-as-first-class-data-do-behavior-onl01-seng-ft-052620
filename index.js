20054531c8fc724f4c6f114bc56f3de2ea0d2aae
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-08-26 11:53:35 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let currentHour = time.split(':')[0];
+  currentHour= parseInt(currentHour)
+  console.log(currentHour)
+  if (currentHour < 12) {
+    return "Good Morning"
+  }
+  else if ( 12 <= currentHour && currentHour < 17) {
+    return "Good Afternoon"
+  }
+  else if ( 17 <= currentHour) {
+    return "Good Evening"
+  }
+};
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
