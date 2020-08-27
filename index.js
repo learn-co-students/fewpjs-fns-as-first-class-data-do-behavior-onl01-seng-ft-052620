11b5c7e3dd8b3fa960cfcad558ffcb86ce212beb
Eyeruz <3yerus@gmail.com>
2020-08-27 19:23:03 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  const timeNow = parseInt(time, 10);
+  if (timeNow < 12) return "Good Morning"
+  if (timeNow > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
+
+
+
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
